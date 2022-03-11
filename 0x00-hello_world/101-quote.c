8b1a17dafa9348ad0290e77c9f17b4bae0893183
SebastianOpiyo <sebamopale@gmail.com>
2022-03-10 18:43:20 -0800

    A program that prints to standard error file.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,21 +3,11 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- * Assignment: print size of datastructures of the computer.
+ * Assignment: print to the standard error.
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	char charType;
-	long longType;
-	long long int longLongIntType;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	fwrite("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n" 39, 1, stderr);
 
 	return (0);
 }
