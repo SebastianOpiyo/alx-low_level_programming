dbf8ef1827956ff3a64b6c6d55121f22422e5844
SebastianOpiyo <sebamopale@gmail.com>
2022-03-10 17:51:24 -0800

    Edit print statement grammer.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long longType;
 	long long int longLongIntType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of long: %zu byte(s)\n", sizeof(longType));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(longLongIntType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
