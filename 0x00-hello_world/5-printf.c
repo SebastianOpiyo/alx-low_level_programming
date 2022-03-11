7c082338debc6e3dbad4f515c43c14a8f952633c
SebastianOpiyo <sebamopale@gmail.com>
2022-03-10 17:22:44 -0800

    Add a printif simple programme.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Assignment: Use printf to print string on the screen.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,4 +3,5 @@
 2. A script that assesmbles and produces an output file.
 3. A script that compiles and creates an executable named file.
 4. A string programme.
-5.  
+5. A printf programme.
+6.  
