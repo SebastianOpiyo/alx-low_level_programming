6ab176ede13608aa938015a36e968802361ccc66
SebastianOpiyo <sebamopale@gmail.com>
2022-03-10 16:49:32 -0800

    Format code as per betty standards.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Use puts to print string on the screen.
+ */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
-	return 0;
+	return (0);
 }
