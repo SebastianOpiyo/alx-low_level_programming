7fc60e930fb27dc9dab5f7132c285be4b9205ad5
SebastianOpiyo <sebamopale@gmail.com>
2022-03-14 14:58:26 -0700

    A a program that prints numbers from 0-9.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function that prints numbers 0-9.
+ * Return: Always 0 (Success)
+ * Print a new line too.
+ */
+
+int main(void)
+{
+
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,4 +4,5 @@ CONDITIONAL & WHILE LOOPS
 2. A program that prints alphabet letters.
 3. A program that prints both the lower case and uppercase alphabet letters.
 4. A program that prints lowercase letters apart from e and q
-5.  
+5. A program that prints numbers from 0-9
+6.   
