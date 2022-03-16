abc2d426b7902fe99d84ac1fe39f52545b2df9ec
SebastianOpiyo <sebamopale@gmail.com>
2022-03-16 14:28:30 -0700

    Assignment 3: print letters 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets from a-z ten times
+ * Return: On success 0
+ * Prints a new line too, and only used _putchar
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	char ch;
+
+	while (counter < 10)
+	{
+		ch = 'a';
+		while (ch < 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,4 +2,5 @@ FUNCTION & NESTED LOOPS
 
 0. Use '_putchar' to print out "_putchar"
 1. A program that prints alphabet letters in small letters using _putchar
-2. 
+2. A program to print alphabet letters from a-z ten times.
+3. 
