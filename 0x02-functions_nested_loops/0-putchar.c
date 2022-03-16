a615976281073bd9fc761dbbc76e282e151c6469
SebastianOpiyo <sebamopale@gmail.com>
2022-03-16 11:41:42 -0700

    Putchar funtion.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - A program that prints "_putchar"
+ * Return: On success (0)
+ * On error (-1)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+FUNCTION & NESTED LOOPS
+
+0. Use '_putchar' to print out "_putchar"
+1.
