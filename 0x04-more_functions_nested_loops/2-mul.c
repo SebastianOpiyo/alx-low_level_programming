40b5f3aa9792ba3cdf1f0c004e4145e6225757f9
SebastianOpiyo <sebamopale@gmail.com>
2022-03-17 14:17:45 -0700

    Multiplier function.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: multiplication result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
