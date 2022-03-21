e3a696710eb9a580f950bc2cfe6df3c4ed6f7bf5
SebastianOpiyo <sebamopale@gmail.com>
2022-03-21 08:59:03 -0700

    Remove strlen.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,19 @@
  * _strlen - a function that returns the length of a string
  * @s: Pointer to the string
  *
- * Return: Always 0
+ * Return: int
  */
 
 int _strlen(char *s)
 {
 	int len;
 
-	len = strlen(*s);
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-	return (0);
+	return (len);
 
 }
