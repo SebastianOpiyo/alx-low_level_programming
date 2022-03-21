8927b00fe8bb45746a60e38fd1f04bb2175a4611
SebastianOpiyo <sebamopale@gmail.com>
2022-03-21 08:23:58 -0700

    A function that swaps var values using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps the values of two integers.
+ * @a: first pointer to the first int
+ * @b: second pointer to the second int
+ *
+ * Return: Always void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 ARRAYS, POINTERS & STRINGS
 
-
+0. Assign a value to an int using a pointer
+1. Swap int variable values using pointers
+2.
