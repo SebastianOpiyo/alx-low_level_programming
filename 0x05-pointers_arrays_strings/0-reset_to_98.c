8050a83076a6dfddb86999d204371c97005ede7d
SebastianOpiyo <sebamopale@gmail.com>
2022-03-21 07:58:11 -0700

    Use pointers to change variable values.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - a func that takes a pointer to an int as param
+ * and updates the value it points to to 98.
+ * @n: pointer parameter
+ * Return: Always void
+ */
+
+void reset_to_98(int *n)
+{
+	_putchar(n);
+	*n = 98;
+	_putchar(n);
+}
+
