3ce1b935a0c1d4a82f8018ecc8680df3312caf6a
SebastianOpiyo <sebamopale@gmail.com>
2022-03-22 04:40:30 -0700

    removed _strlen and implemented strlen.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,19 @@ void print_rev(char *s)
 	int i;
 	int j;
 	unsigned char a;
-	unsigned int len = _strlen((const char *)s);
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
 	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
 		a = s[i];
 		s[i] = s[j];
 		s[j] = a;
+		_putchar(s[j]);
 
 	}
 	_putchar('\n');
