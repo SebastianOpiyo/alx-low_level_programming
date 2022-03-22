22a04be1a4f1f551966d8cec6b9369888593ad09
SebastianOpiyo <sebamopale@gmail.com>
2022-03-22 10:31:52 -0700

    fix printf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i > (n - 1))
+		if (i < (n - 1))
 		{
-			break;
+			printf(", ");
 		}
 		else
 		{
-			printf("%d, ", a[i]);
+			printf("%d", a[i]);
 		}
 	}
 	printf("\n");
