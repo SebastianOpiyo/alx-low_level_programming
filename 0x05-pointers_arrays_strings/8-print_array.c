e4e819cb6a4390bf7aa643d02d02d09c3b4a5a05
SebastianOpiyo <sebamopale@gmail.com>
2022-03-22 10:36:39 -0700

    fix space and comma prints

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
+		printf("%d", i);
 		if (i < (n - 1))
 		{
 			printf(", ");
-		}
-		else
-		{
-			printf("%d", a[i]);
 		}
 	}
 	printf("\n");
