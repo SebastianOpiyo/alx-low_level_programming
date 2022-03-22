813ac68520fad9dbd9a8ded3683f92f891b81f3a
SebastianOpiyo <sebamopale@gmail.com>
2022-03-22 04:27:57 -0700

    Enhancement, and bug fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,17 @@
 void print_rev(char *s)
 {
 	int i;
-	
-	i = _strlen(s);
-	
-	while (s[i] != 0)
+	int j;
+	unsigned char a;
+	unsigned int len = _strlen((const char *)s);
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
-		_putchar(s[i]);
+		a = s[i];
+		s[i] = s[j];
+		s[j] = a;
+
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
